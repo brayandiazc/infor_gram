58962cbef8f4782571ec703812325d5665e714fa
Brayan Diaz C <brayandiazc@gmail.com>
2024-10-12 08:49:41 -0500

    Annotate gem

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ group :test do
 end
 
 gem "devise", "~> 4.9"
+
+gem "annotate", "~> 3.2"
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  content     :text
+#  location_id :bigint           not null
+#  user_id     :bigint           not null
+#  available   :boolean          default(TRUE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Article < ApplicationRecord
   belongs_to :location
   belongs_to :user
